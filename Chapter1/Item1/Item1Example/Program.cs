b866f561d057b606675de6a63770520a03f9fd2f
Bellhoon-__- <ilbora@naver.com>
2024-05-20 23:50:06 +0900

    docs: chapter1 - item1
    지역변수를 선언할 때는 var를 사용하는 것이 낫다

---
+++ Chapter1/Item1/Item1Example/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        // var를 사용하여 초기화되는 변수
+        var data = GetData();
+        Console.WriteLine("데이터 타입: " + data.GetType().Name);
+        
+        // 작업을 수행하는 과정에서 변수의 실제 타입을 명확히 인지하지 못하는 경우
+        if (data is List<int>)
+        {
+            Console.WriteLine("List<int> 타입으로 처리");
+            // List<int>로 예상되는 로직 처리
+            foreach (var num in (List<int>)data)
+            {
+                Console.Write(num + " ");
+            }
+        }
+        else
+        {
+            Console.WriteLine("예상하지 못한 데이터 타입: " + data.GetType().Name);
+        }
+        Console.WriteLine();
+    }
+
+    // 데이터 반환 타입이 명확하지 않은 메소드
+    private static object GetData()
+    {
+        // 반환 타입이 List<int>인 경우
+        return new List<int> { 1, 2, 3, 4, 5 };
+
+        // 반환 타입이 다른 경우를 고려하지 않는다면,
+        // return new List<string> { "one", "two", "three" };
+    }
+}

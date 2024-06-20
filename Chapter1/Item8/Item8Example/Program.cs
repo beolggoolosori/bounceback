0a63a77a9ee41cb3c91090f27798cbd37fb92ecd
Bellhoon-__- <ilbora@naver.com>
2024-06-20 09:20:52 +0900

    docs: chapter1 - item8 이벤트 호출 시에는 null 조건 연산자를 사용하라

---
+++ Chapter1/Item8/Item8Example/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    public static event EventHandler MultipleEvent;
+
+    public static void Main()
+    {
+        // 여러 개의 이벤트 핸들러 등록
+        MultipleEvent += FirstHandler;
+        MultipleEvent += SecondHandler;
+
+        // 이벤트 호출
+        OnMultipleEvent();
+
+        // 첫 번째 핸들러만 제거
+        MultipleEvent -= FirstHandler;
+
+        // 이벤트 재호출
+        OnMultipleEvent();
+    }
+
+    private static void FirstHandler(object sender, EventArgs e)
+    {
+        Console.WriteLine("첫 번째 핸들러 호출됨.");
+    }
+
+    private static void SecondHandler(object sender, EventArgs e)
+    {
+        Console.WriteLine("두 번째 핸들러 호출됨.");
+    }
+
+    protected static void OnMultipleEvent()
+    {
+        // null 조건 연산자를 사용한 이벤트 호출
+        MultipleEvent?.Invoke(null, EventArgs.Empty);
+    }
+}

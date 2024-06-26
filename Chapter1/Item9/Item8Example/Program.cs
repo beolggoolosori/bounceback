24f78d6e80791a33f3076ed1febdf61548c67629
Bellhoon-__- <ilbora@naver.com>
2024-06-26 13:57:22 +0900

    docs: chapter1 - item9 박싱과 언박싱을 최소화하라

---
+++ Chapter1/Item9/Item8Example/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    public static event EventHandler MultipleEvent;
+
+    public static void Main()
+    {
+        int firstNumber = 3;
+        int secondNumber = 4;
+        int thirdNumber = 5;
+
+        Console.WriteLine($"A few numbers:{firstNumber}, {secondNumber}, {thirdNumber}");
+        Console.WriteLine($"A few numbers:{firstNumber.ToString()},{secondNumber.ToString()}, {thirdNumber.ToString()}");
+    }
+}

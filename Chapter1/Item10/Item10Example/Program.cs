51b5b6b0447fb69a1f4cc3af5ec78b21a3d9afe0
Bellhoon-__- <ilbora@naver.com>
2024-06-27 17:23:31 +0900

    docs: chapter1 - item10 : 베이스 클래스가 업그레이드된 경우에만 new 한정자를 사용해라

---
+++ Chapter1/Item10/Item10Example/Program.cs
@@ -0,0 +1,29 @@
+﻿public class BaseClass
+{
+    public void Display()
+    {
+        Console.WriteLine("BaseClass Display");
+    }
+}
+
+public class DerivedClass : BaseClass
+{
+    public new void Display()
+    {
+        Console.WriteLine("DerivedClass Display");
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        BaseClass objBase = new BaseClass();
+        DerivedClass objDerived = new DerivedClass();
+        BaseClass objDerivedAsBase = new DerivedClass();
+
+        objBase.Display();
+        objDerived.Display();
+        objDerivedAsBase.Display();
+    }
+}

0a55ce68b6091cd6c569b0ee9fc32782ccedcf55
Bellhoon-__- <ilbora@naver.com>
2024-07-10 18:25:18 +0900

    docs: chapter2 - item14 초기화 코드가 중복되는 것을 최소화하라

---
+++ Chapter2/Item14/Example/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Creating an instance of MyClass.");
+        MyClass instance = new MyClass(5);
+        Console.WriteLine($"Value of _number: {instance.Number}");
+    }
+}
+
+public class BaseClass
+{
+    public BaseClass()
+    {
+        Console.WriteLine("BaseClass constructor called.");
+    }
+}
+
+public class MyClass : BaseClass
+{
+    // 정적 필드 초기화
+    private static int _staticNumber = InitializeStaticNumber();
+
+    // 인스턴스 필드 초기화
+    private int _number = 10;
+
+    // 정적 생성자
+    static MyClass()
+    {
+        Console.WriteLine("Static constructor called.");
+        _staticNumber += 1;
+    }
+
+    // 기본 생성자
+    public MyClass() : this(42) // 생성자 체이닝
+    {
+        Console.WriteLine("MyClass default constructor called.");
+    }
+
+    // 매개변수 있는 생성자
+    public MyClass(int number)
+    {
+        Console.WriteLine("MyClass constructor with parameter called.");
+        _number = number;
+    }
+
+    // 정적 필드 초기화 메서드
+    private static int InitializeStaticNumber()
+    {
+        Console.WriteLine("Static field initializer called.");
+        return 30;
+    }
+
+    public int Number
+    {
+        get { return _number; }
+    }
+}

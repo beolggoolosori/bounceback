d163247658251e118e5b1c06ebd6e998f015a01c
Bellhoon-__- <ilbora@naver.com>
2024-07-11 18:01:02 +0900

    docs: chapter2 - item16 생성자 내에서는 절대로 가상 함수를 호출하지 말라

---
+++ Chapter2/Item16/Example/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class BaseClass
+{
+    public BaseClass()
+    {
+        Init();
+    }
+
+    public virtual void Init()
+    {
+        Console.WriteLine("Base Init");
+    }
+}
+
+public class DerivedClass : BaseClass
+{
+    private int _number = 5;
+
+    public DerivedClass()
+    {
+    }
+
+    public override void Init()
+    {
+        Console.WriteLine($"Derived Init: {_number}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BaseClass obj = new DerivedClass();
+    }
+}

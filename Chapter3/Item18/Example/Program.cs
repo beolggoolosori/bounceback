a59c8ab72afe66579f050cd33a16082b15133cad
Bellhoon-__- <ilbora@naver.com>
2024-07-15 09:29:12 +0900

    docs: chapter3 - item18 반드시 필요한 제약 조건만 설정하라

---
+++ Chapter3/Item18/Example/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Program
+{
+    // 제네릭 메서드 정의, T는 IComparable 인터페이스를 구현해야 함
+    public static T GetMax<T>(T a, T b) where T : IComparable
+    {
+        return a.CompareTo(b) > 0 ? a : b;
+    }
+
+    public static void Main()
+    {
+        // int는 IComparable 인터페이스를 구현함
+        int maxInt = GetMax(5, 10);
+        Console.WriteLine($"Max Int: {maxInt}");
+
+        // string 또한 IComparable 인터페이스를 구현함
+        string maxString = GetMax("apple", "banana");
+        Console.WriteLine($"Max String: {maxString}");
+    }
+}

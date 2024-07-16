9c07d630969e492cb44d8f94211e245aa87b71be
Bellhoon-__- <ilbora@naver.com>
2024-07-16 10:14:02 +0900

    docs: chapter3 - Item19 런타임에 타입을 확인하여 최적의 알고리즘을 사용하라

---
+++ Chapter3/Item19/Example/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+class GenericExample<T>
+{
+    private T data;
+
+    public GenericExample(T value)
+    {
+        data = value;
+    }
+
+    public void DisplayData()
+    {
+        Console.WriteLine($"Data: {data}");
+    }
+}
+
+class NonGenericExample
+{
+    private object data;
+
+    public NonGenericExample(object value)
+    {
+        data = value;
+    }
+
+    public void DisplayData()
+    {
+        Console.WriteLine($"Data: {data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // 제네릭을 사용하는 예
+        GenericExample<int> intGeneric = new GenericExample<int>(123);
+        intGeneric.DisplayData();  // 출력: Data: 123
+
+        GenericExample<string> stringGeneric = new GenericExample<string>("Hello Generic");
+        stringGeneric.DisplayData();  // 출력: Data: Hello Generic
+
+        // 제네릭을 사용하지 않는 예
+        NonGenericExample intNonGeneric = new NonGenericExample(456);
+        intNonGeneric.DisplayData();  // 출력: Data: 456
+
+        NonGenericExample stringNonGeneric = new NonGenericExample("Hello Non-Generic");
+        stringNonGeneric.DisplayData();  // 출력: Data: Hello Non-Generic
+    }
+}

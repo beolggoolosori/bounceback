cdf7618baa47e5d3e7aae1b631784208d723a374
Bellhoon-__- <ilbora@naver.com>
2024-07-17 09:30:27 +0900

    docs: chapter3 - Item20  IComparable<T>와 IComaparer<T> 를 이용하여 객체의 선후 관계를 정의하라

---
+++ Chapter3/Item20/Example/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+
+// IComparable<T> 사용 예제
+public class Employee : IComparable<Employee>
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+
+    public int CompareTo(Employee other)
+    {
+        if (other == null) return 1;
+        return this.Id.CompareTo(other.Id);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Employee[] employees = {
+            new Employee {Name = "John", Id = 2},
+            new Employee {Name = "Jane", Id = 1},
+            new Employee {Name = "Doe", Id = 3}
+        };
+
+        Array.Sort(employees);
+        foreach (var emp in employees)
+        {
+            Console.WriteLine($"{emp.Name}, {emp.Id}");
+        }
+    }
+}
+
+using System;
+using System.Collections.Generic;
+
+public class Employee
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+}
+
+
+// IComparer<T> 사용 예제
+public class SortByName : IComparer<Employee>
+{
+    public int Compare(Employee x, Employee y)
+    {
+        return String.Compare(x.Name, y.Name);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Employee> employees = new List<Employee> {
+            new Employee {Name = "John", Id = 2},
+            new Employee {Name = "Jane", Id = 1},
+            new Employee {Name = "Doe", Id = 3}
+        };
+
+        employees.Sort(new SortByName());
+        foreach (var emp in employees)
+        {
+            Console.WriteLine($"{emp.Name}, {emp.Id}");
+        }
+    }
+}

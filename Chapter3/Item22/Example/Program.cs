27820df400226f1b522a533cc04513f355b54310
beolggoolosori <ilbora@naver.com>
2024-07-19 00:23:18 +0900

    docs: 디렉토리 수정

---
+++ Chapter3/Item22/Example/Program.cs
@@ -0,0 +1,18 @@
+﻿public class ResourceHolder<T> : IDisposable where T : class
+{
+    private T resource;
+
+    public ResourceHolder(T resource)
+    {
+        this.resource = resource;
+    }
+
+    public void Dispose()
+    {
+        // 타입 매개변수의 인스턴스가 IDisposable을 구현하는지 확인
+        if (resource is IDisposable disposable)
+        {
+            disposable.Dispose();
+        }
+    }
+}

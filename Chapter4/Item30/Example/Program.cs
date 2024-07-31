883faf9992acc849016dc32ecc8bd09f76b1ceb4
Bellhoon-__- <ilbora@naver.com>
2024-07-31 09:19:19 +0900

    docs: Chapter4 - Item30 루프보다 쿼리 구문이 낫다

---
+++ Chapter4/Item30/Example/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // LINQ 쿼리 구문을 사용한 예제
+        var evenNumbersUsingLinq = numbers.Where(n => n % 2 == 0);
+
+        Console.WriteLine("Even Numbers (using LINQ):");
+        foreach (var number in evenNumbersUsingLinq)
+        {
+            Console.WriteLine(number);
+        }
+
+        // LINQ를 사용한 정렬 예제
+        var sortedNumbersUsingLinq = numbers.OrderBy(n => n);
+
+        Console.WriteLine("Sorted Numbers (using LINQ):");
+        foreach (var number in sortedNumbersUsingLinq)
+        {
+            Console.WriteLine(number);
+        }
+
+        // 루프를 사용한 예제
+        List<int> evenNumbersUsingLoops = new List<int>();
+        foreach (var number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                evenNumbersUsingLoops.Add(number);
+            }
+        }
+
+        Console.WriteLine("Even Numbers (using loops):");
+        foreach (var number in evenNumbersUsingLoops)
+        {
+            Console.WriteLine(number);
+        }
+
+        // 루프를 사용한 정렬 예제
+        List<int> sortedNumbersUsingLoops = new List<int>(numbers);
+        sortedNumbersUsingLoops.Sort();
+
+        Console.WriteLine("Sorted Numbers (using loops):");
+        foreach (var number in sortedNumbersUsingLoops)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
---
+++ Chapter4/Item30/Example2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = Enumerable.Range(1, 1000000000).ToList(); // 큰 데이터셋 생성
+
+        // LINQ 쿼리 구문 실행 시간 측정
+        Stopwatch stopwatch = new Stopwatch();
+
+        stopwatch.Start();
+        var evenNumbersUsingLinq = numbers.Where(n => n % 2 == 0).ToList();
+        stopwatch.Stop();
+        long linqElapsedTime = stopwatch.ElapsedMilliseconds;
+
+        Console.WriteLine("LINQ Execution Time: " + linqElapsedTime + " ms");
+
+        // 루프를 사용한 실행 시간 측정
+        stopwatch.Reset();
+
+        stopwatch.Start();
+        List<int> evenNumbersUsingLoops = new List<int>();
+        foreach (var number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                evenNumbersUsingLoops.Add(number);
+            }
+        }
+        stopwatch.Stop();
+        long loopsElapsedTime = stopwatch.ElapsedMilliseconds;
+
+        Console.WriteLine("Loops Execution Time: " + loopsElapsedTime + " ms");
+
+        // 결과 비교
+        Console.WriteLine("Number of even numbers using LINQ: " + evenNumbersUsingLinq.Count);
+        Console.WriteLine("Number of even numbers using loops: " + evenNumbersUsingLoops.Count);
+    }
+}

d9ddfa46d8560cf0449d3d4326b1326dc16a7745
Bellhoon-__- <ilbora@naver.com>
2024-08-03 11:42:38 +0900

    docs: Chapter4 - Item33 필요한 시점에 필요한 요소를 생성하라

---
+++ Chapter4/Item33/Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class FibonacciSequence
+{
+    public static IEnumerable<long> Generate(int count)
+    {
+        long previous = 0, next = 1;
+        for (int i = 0; i < count; i++)
+        {
+            yield return previous;
+            long temp = next;
+            next = previous + next;
+            previous = temp;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var number in FibonacciSequence.Generate(10))
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
